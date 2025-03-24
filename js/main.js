4f7ec711c384b78728b6863a2a7029c3e054b64b
William <strandqvist.william@gmail.com>
2025-03-24 12:09:12 +0100

    check dokumentation document at march 23 2025

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
+// developer debug mode
+let debug_mode = true;
+
+
+
 // ct = Class Toggler
 // add event listener to all elements with the "affected_element-toggle" class in order to allow code to be executed when they are pressed
 document.querySelectorAll(".ct-toggler").forEach((toggle) => {
   toggle.addEventListener("click", function (event) {
-    // find the closest relative/anscestor to this element that has the "ct-container" class and input that in to a variable
-    let container = this.closest(".ct-container");
+
+    let container;
+
+    //check if clicked element is a ct-container
+    if (this.classList.contains(".ct-container")) {
+      container = this
+      console.error("Error: clicked object is container");
+    } else {
+      // find the closest relative/anscestor to this element that has the "ct-container" class and input that in to a variable
+      container = this.closest(".ct-container");
+    }
+    
+    
 
     // if the code didn't find any relative/anscestor to this element with the "ct-container" class, meaning the variable value is null, then
     if (container == null) {
       // notify of the issue
-      console.error("Error: a .ct-toggler is missing a .ct-container!");
-
-      // end this function
+      if (debug_mode) {console.error("Error: a .ct-toggler is missing a .ct-container!", this.outerHTML);}
       return;
     }
 
@@ -37,23 +51,22 @@ document.querySelectorAll(".ct-toggler").forEach((toggle) => {
 });
 
 
-// deactivate class toggler if a click occurs somewhere other that the container
+// deactivate class togglers  if a click occurs somewhere other that the container
+// and they have the class "ct-deactivateOnOutsideClick"
 
 // run function for every clicked element with class "ct-container"
 document.addEventListener("click", function (event) {
-  document.querySelectorAll(".ct-container").forEach((container) => {
-
+  document.querySelectorAll('.ct-container.ct-deactivateOnOutsideClick').forEach((container) => {
+    console.log("Container found with both classes", container);
     // if click didn't occur in the container
     if (!container.contains(event.target)) {
 
-        // deactivate container
+      // deactivate container
       container.classList.remove("ct-active");
       container.classList.add("ct-unactive");
 
       //find elements that are affected by the class toggler and are active
-      let activeElements = container.querySelectorAll(
-        ".ct-active"
-      );
+      let activeElements = container.querySelectorAll(".ct-active");
 
       //deactivate all active classes
       activeElements.forEach((el) => {
@@ -64,22 +77,49 @@ document.addEventListener("click", function (event) {
   });
 });
 
+// toggle automatic scroll setting
+let auto_scroll_enabled = true; 
+document.querySelectorAll(".automatic-scroll-toggle").forEach((toggle) => {
+  toggle.addEventListener("click", function (event) {
+    auto_scroll_enabled = !auto_scroll_enabled;
+  });
+});
 
 //scroll to the IB when it is clicked
 document.querySelectorAll(".ib-expand-button").forEach((toggle) => {
   toggle.addEventListener("click", function (event) {
-    //make sure the scroll
-    setTimeout(() => {
-      //find the closest "IB" to the IB_ExpandButton
-      let container = this.closest(".ib");
+    // only scroll if the setting is enabled
+    if (auto_scroll_enabled) {
+      //make sure the scroll happeneds after the animation
+      setTimeout(() => {
+        //find the closest "IB" to the ib-expand-button
+        let container = this.closest(".ib");
 
-      //make sure it was found to prevent exeption
-      if (container) {
-        //scroll to it
-        container.scrollIntoView({ behavior: "smooth", block: "start" });
-      }
-    }, 300); // delay before scroll takes affect to avoid scrolling to an item that is changing position
+        //make sure it was found to prevent exeption
+        if (container) {
+          //scroll to it
+          container.scrollIntoView({ behavior: "smooth", block: "start" });
+        }
+      }, 300); // delay before scroll takes affect to avoid scrolling to an item that is changing position
+    }
   });
 });
 
-// document.querySelectorAll("*").forEach(el => el.style.outline = "1px solid red");
+
+// show element borders (debug)
+if (debug_mode) {
+  let show_borders = false;
+  document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 'b') {
+      show_borders = !show_borders;
+
+      document.querySelectorAll("*").forEach((el) => {
+        if (show_borders) {
+          el.classList.add("show-borders");
+        }else{
+          el.classList.remove("show-borders");
+        }
+      })
+    }
+  });
+}
